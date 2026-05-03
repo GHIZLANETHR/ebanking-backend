2f97b0f75a1af8ac1ecf4ccb71c51d2d04005f10
Ghizlanethr <touahrighizlane98@gmail.com>
2026-05-03 11:19:28 +0100

    BankAccountSetvice avec les excéptions

--- src/main/java/org/example/ebankingbackend/EbakingBackendApplication.java
+++ src/main/java/org/example/ebankingbackend/EbakingBackendApplication.java
@@ -1,5 +1,6 @@
 package org.example.ebankingbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class EbakingBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(EbakingBackendApplication.class, args);
     }
+
 
 }
